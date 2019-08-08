f7281ac32aa648e1056fab8e4b29eee01aa8e351
Patrick Yambrick <patrickyambrick@gmail.com>
2019-08-08 15:28:41 -0400

    Add footer component

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,11 @@ export default class Footer extends Component {
     return(
       <footer className='page-footer row'>
         <div className='container-fluid'>
-          <div className='row'>
-            Made with <a href='#'>React</a>, <a href='#'>Bootstrap</a>, and <a href='#'>PokeAPI</a>
+          <div className='row center-row'>
+            Made with <a href='https://reactjs.org/'>React</a>, <a href='https://getbootstrap.com/'>Bootstrap</a>, and <a href='https://pokeapi.co/'>PokeAPI</a>
           </div>
-          <div className='row'>
-            Check it out on <a href='#'>Github</a>!
+          <div className='row center-row'>
+            Check it out on <a href='https://github.com/YouGuavas/pokedex'>Github</a>!
           </div>
         </div>
       </footer>
